d0cc3473be0b9f286e89921f9ff72a34c1eedcb0
Reyoth <serialkiwi@gmail.com>
2022-02-28 11:51:42 +0100

    Finalisation du programme morse

--- liste_morse.py
+++ liste_morse.py
@@ -1,5 +1,7 @@
-alphabet=["a","b","c","d","e","f","g","h","i",
-"j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+alphabet=["a",
+"b","c","d","e","f","g","h","i",
+"j","k","l","m","n","o","p","q",
+"r","s","t","u","v","w","x","y","z"," "]
 
 morse=[".-",
 "-...",
@@ -26,7 +28,25 @@ morse=[".-",
 ".--",
 "-..-",
 "-.--",
-"--.."]
+"--..",
+" "]
+
+#encode lettre > code morse
+def encode(lettre):
+    # on cherche l'index de la lettre dans la liste alphabet
+    index = alphabet.index(lettre)
+    #on recherche le code morse correspondant a l'index
+    code_morse= morse[index]
+    #on renvoit le code morse trouve
+    return code_morse
+
+#decode code morse > lettre
+def decode(code):
+    index = morse.index(code)
+    lettre = alphabet[index]
+    return lettre
+
+
 
 #print(alphabet.__len__())
 #print(morse.__len__())
--- programme_morse.py
+++ programme_morse.py
@@ -7,9 +7,32 @@
 
 import liste_morse
 
-print("entrez un caractere de a->z")
-lettre= input(">>>")
+# encoder un mots en morse
+print("entrez un un mots")
+mots= input(">>>")
+code=""
+# bonjour
+for lettre in mots:
+    code = code + liste_morse.encode(lettre)
+    code = code + " "
+print(code)
 
-index = liste_morse.alphabet.index(lettre)
-print(liste_morse.morse[index])
+#------------------------
+# on decode un code morse en mots
+print("entrez un code morse")
+code = input(">>>")
+mots=""
+#"--- -. ---"
+#[---, -., ---]
+# On decoupe notre code en liste de code separe par un espace
+liste_code = str.split(code," ")
+#on affiche le resultat de la decoupe
+print(liste_code)
+# pour chaque element de la liste_code, on recupere la lettre correspondante et on
+# la stoque dans la viriable mots
+for element in liste_code:
+    mots = mots + liste_morse.decode(element)
+
+print(mots)
+
 
